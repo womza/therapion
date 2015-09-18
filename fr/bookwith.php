0b7854b418208c2a552de7bad766a51bfa84f075
William Ortega <william.ortega@outlook.com>
2015-09-18 00:53:03 -0600

    Francais url friendly

--- fr/bookwith.php
+++ fr/bookwith.php
@@ -2,6 +2,7 @@
 require_once '../db_conn.php';
 $psicologo=get_psicologo($_GET['id_psicologo']);
 $metodos=get_psicologo_metodos($_GET['id_psicologo']); 
+$psicologo_name = get_psicologo_name_url($psicologo);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -41,13 +42,13 @@ $metodos=get_psicologo_metodos($_GET['id_psicologo']);
 						<div class="wrap-buttons">	
 							<?php foreach($metodos as $metodo): 
 							if($metodo['titulo']=='email'):?>											
-							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/fr/formemail.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultation par <br />Courrier électronique</a> 
+							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/fr/<?php echo $psicologo_name ?>/email">Consultation par <br />Courrier électronique</a> 
 							<?php elseif ($metodo['titulo']=='chat'): ?>
-							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/fr/formchat.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultation par <br />Chat</a> 
+							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/fr/<?php echo $psicologo_name ?>/chat">Consultation par <br />Chat</a> 
 							<?php elseif ($metodo['titulo']=='phone'): ?>
-							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/fr/formphone.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultation par <br />Téléphone</a> 
+							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/fr/<?php echo $psicologo_name ?>/telefoon">Consultation par <br />Téléphone</a> 
 							<?php else: ?>
-							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/fr/formvideoconference.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultation par <br />Vidéoconférence</a>
+							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/fr/<?php echo $psicologo_name ?>/videoconferentie">Consultation par <br />Vidéoconférence</a>
 						<?php endif; endforeach; ?>
 						</div>
 					</div>
--- fr/consultation.php
+++ fr/consultation.php
@@ -47,16 +47,17 @@
 						//exit(print_r($psicologos->fetch_array(MYSQLI_ASSOC)));
 						//echo print_r($psicologos);
 						while($psicologo=$psicologos->fetch_array(MYSQLI_ASSOC)):
+							$psicologo_name = get_psicologo_name_url($psicologo);
 						 ?>
 						
 						<div class="profile">
  
  							<div class="photo <?php echo ($i==0)?'lft':'rgt' ?>">
 								<img src="../assets/images/profiles/<?php echo $psicologo['foto'] ?>" height="150" width="100">							 
-								<a class="standardsButton " href="./bookwith.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultez avec moi</a>
+								<a class="standardsButton " href="./<?php echo $psicologo_name ?>">Consultez avec moi</a>
 							</div>
 							
-							<h4><a href="./bookwith.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>"><?php echo $psicologo['nombres_apellidos'] ?></a></h4>
+							<h4><a href="./<?php echo $psicologo_name ?>"><?php echo $psicologo['nombres_apellidos'] ?></a></h4>
 							
 							<p><b>  
 							<?php echo $psicologo['titulo'] ?>
@@ -93,16 +94,17 @@
 						$i=0;
 						//echo print_r($psicologos);
 						foreach($psicologos as $psicologo):
+							$psicologo_name = get_psicologo_name_url($psicologo);
 						 ?>
 						
 						<div class="profile">
  
  							<div class="photo <?php echo ($i==0)?'lft':'rgt' ?>">
 								<img src="../assets/images/profiles/<?php echo $psicologo['foto'] ?>" height="150" width="100">							 
-								<a class="standardsButton " href="./bookwith.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Consultez avec moi</a>
+								<a class="standardsButton " href="./<?php echo $psicologo_name ?>">Consultez avec moi</a>
 							</div>
 							
-							<h4><a href="./bookwith.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>"><?php echo $psicologo['nombres_apellidos'] ?></a></h4>
+							<h4><a href="./<?php echo $psicologo_name ?>"><?php echo $psicologo['nombres_apellidos'] ?></a></h4>
 							
 							<p><b>  
 							<?php echo $psicologo['titulo'] ?>
