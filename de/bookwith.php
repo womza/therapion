354d04e4f5d3dd403dc9494a233ca455dd42f128
William Ortega <william.ortega@outlook.com>
2015-09-18 00:38:41 -0600

    Deutsch url friendly

--- de/bookwith.php
+++ de/bookwith.php
@@ -2,6 +2,7 @@
 require_once '../db_conn.php';
 $psicologo=get_psicologo($_GET['id_psicologo']);
 $metodos=get_psicologo_metodos($_GET['id_psicologo']); 
+$psicologo_name = get_psicologo_name_url($psicologo);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
@@ -41,13 +42,13 @@ $metodos=get_psicologo_metodos($_GET['id_psicologo']);
 						<div class="wrap-buttons">	
 							<?php foreach($metodos as $metodo): 
 							if($metodo['titulo']=='email'):?>											
-							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/de/formemail.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Beratung per <br />E-Mail</a> 
+							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/de/<?php echo $psicologo_name ?>/email">Beratung per <br />E-Mail</a> 
 							<?php elseif ($metodo['titulo']=='chat'): ?>
-							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/de/formchat.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Beratung per <br />Chat</a> 
+							<a class="standardsButton" href="<?php echo get_credentials()['site_root']?>/de/<?php echo $psicologo_name ?>/chat">Beratung per <br />Chat</a> 
 							<?php elseif ($metodo['titulo']=='phone'): ?>
-							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/de/formphone.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Beratung per <br />Telefon</a> 
+							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/de/<?php echo $psicologo_name ?>/telefon">Beratung per <br />Telefon</a> 
 							<?php else: ?>
-							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/de/formvideoconference.php?id_psicologo=<?php echo $psicologo['id_psicologo'] ?>">Beratung per <br />Videokonferenz</a>
+							<a class="standardsButton wm" href="<?php echo get_credentials()['site_root']?>/de/<?php echo $psicologo_name ?>/videokonferenz">Beratung per <br />Videokonferenz</a>
 						<?php endif; endforeach; ?>
 						</div>
 					</div>
